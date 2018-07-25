baabfd5c741b4267f0520730123ce783a7736edc
Majid Ansari <33059093+Majid023@users.noreply.github.com>
2018-07-25 08:19:08 +0530

    update and add new data

--- app/routers/makeimage.js
+++ app/routers/makeimage.js
@@ -1,33 +1,44 @@
 var express = require("express");
 var router = express.Router();
 var jimp = require("jimp");
+var url = require("url");
+var data = require("../data/simple data.json");
+var uc = require("upper-case");
 
-var fileName = "C:/Users/DELL/Documents/GitHub/Learn-node/app/public/images/top-wallpaper.jpg";
-var copfile ="C:/Users/DELL/Documents/GitHub/Learn-node/app/public/images/hello.jpg";
+var fileName = "C:/Users/DELL/Documents/GitHub/Learn-node/app/public/images/top-wallpaper.png";
+var copfile ="C:/Users/DELL/Documents/GitHub/Learn-node/app/public/images/hello.png";
 
 var imageCaption = 'Image caption';
 var loadedImage;
 
-router.get("/makeimage",function(req,res){
-    res.render("makeimg",{
-        pageTitle: "makeimg"
-    });
-});
+//router.get("/makeimage",function(req,res){
+    //res.render("makeimg",{
+      //  pageTitle: "makeimg"
+    //});
+    
+//});
 
-router.get("/makeimage/:string",function(req,res){
+router.get("/makeimage",function(req,res){
+    var q =url.parse(req.url,true).query;
+    var text = q.message;
+
     jimp.read(fileName).then(function(image){
         loadedImage = image;
        
-        return jimp.loadFont(jimp.FONT_SANS_32_WHITE);
+        return jimp.loadFont(jimp.FONT_SANS_16_BLACK);
     }).then(function(font){
       
-        loadedImage.print(font,100,20,req.params.string).write(copfile);
+        loadedImage.print(font,30,30,uc(data.title),340)
+        loadedImage.print(font,50,200,data.description,500).write(copfile);
+        res.render("makeimg1",{
+            pageTitle: "text convert into image"
+        });
     }).catch(function(err){
         
         console.error(err);
 
     });
     
-    res.send( '<img src="/images/hello.jpg" alt="img" style="padding:0;margin:0;height:100%;width:100%"/>');
+   
 });
 module.exports = router;
